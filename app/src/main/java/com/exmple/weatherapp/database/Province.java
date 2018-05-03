42589ad685bfc5889d5390cf12358f74f070c7bc
hogan12345 <hogan1234@126.com>
2018-05-03 22:30:53 +0800

    完成遍历

--- app/src/main/java/com/exmple/weatherapp/database/Province.java
+++ app/src/main/java/com/exmple/weatherapp/database/Province.java
@@ -1,10 +1,38 @@
 package com.exmple.weatherapp.database;
 
-import DataSupport;
+
+import org.litepal.crud.DataSupport;
 
 /**
  * Created by Jason on 2018/5/2.
  */
 
 public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
 }
---
+++ app/src/main/java/com/exmple/weatherapp/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.exmple.weatherapp.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Jason on 2018/5/2.
+ */
+
+public class HttpUtil {
+    public static void sendOKHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
